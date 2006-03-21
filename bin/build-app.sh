b780bff3968d491690f33b6cf57e809826e962da
sdunlop <sdunlop@users.sourceforge.net>
2006-03-21 07:43:28 +0100

    Generalized paths for build-app.sh and build-dir.sh
    darcs-hash:20060321064328-da191-c996bc91f230a73d57edba2f228c37562d980ba1

--- bin/build-app.sh
+++ bin/build-app.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
-
+BIN=`dirname $0`
+LIB=`dirname $BIN`/lib
 STUB=$1
 PROG=$2
 OUTP=$3
 
 if [ x$USE_GUILE != x ]; then
-    DO_SCHEME="guile --debug -l lib/run-guile.scm -e guile-main -s"
+    DO_SCHEME="guile --debug -l $LIB/run-guile.scm -e guile-main -s"
 elif [ x$USE_MZSCHEME != x ]; then
-    DO_SCHEME="mzscheme -M errortrace -f lib/run-mzscheme.scm -C"
+    DO_SCHEME="mzscheme -M errortrace -f $LIB/run-mzscheme.scm -C"
 else
     echo "You must define USE_GUILE, USE_MZSCHEME or MOSC prior to invoking"
     echo "build-app.sh"
     exit 1
 fi
 
-DEPS=`$DO_SCHEME bin/manifest.ms $PROG`
+DEPS=`$DO_SCHEME $BIN/manifest.ms $PROG`
 echo $DEPS
 DEPS_MO=""
 RE_GLUE=""
@@ -24,10 +25,10 @@ for DEP in $DEPS; do
     DEP_MO=$DEP.mo
     if [ ! -s $DEP_MO ]||[ $DEP_MS -nt $DEP_MO ]; then
         echo "Compiling $DEP_MS to $DEP_MO.."
-        $DO_SCHEME bin/mosc.ms $DEP || exit 1
+        $DO_SCHEME $BIN/mosc.ms $DEP || exit 1
         RE_GLUE="yes"
     fi
     DEPS_MO="$DEPS_MO $DEP_MO"
 done
 
-./bin/glue $STUB $DEPS_MO $OUTP && chmod 0755 $OUTP || exit 2
+./$BIN/glue $STUB $DEPS_MO $OUTP && chmod 0755 $OUTP || exit 2
--- bin/build-dir.sh
+++ bin/build-dir.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-
+BIN=`dirname $0`
+LIB=`dirname $BIN`/lib
 LIBDIR=$1
 
 if [ x$USE_GUILE != x ]; then
-    DO_SCHEME="guile --debug -l lib/run-guile.scm -e guile-main -s"
+    DO_SCHEME="guile --debug -l $LIB/run-guile.scm -e guile-main -s"
 elif [ x$USE_MZSCHEME != x ]; then
-    DO_SCHEME="mzscheme -M errortrace -f lib/run-mzscheme.scm -C"
+    DO_SCHEME="mzscheme -M errortrace -f $LIB/run-mzscheme.scm -C"
 else
     echo "You must define USE_GUILE, USE_MZSCHEME or MOSC prior to invoking"
     echo "build-app.sh"
@@ -21,7 +22,7 @@ for LIB_MS in $LIBS; do
     LIB_MO=$LIB.mo
     if [ ! -s $LIB_MO ]||[ $LIB_MS -nt $LIB_MO ]; then
         echo "Compiling $LIB_MS to $LIB_MO.."
-        $DO_SCHEME bin/mosc.ms $LIB || exit 1
+        $DO_SCHEME $BIN/mosc.ms $LIB || exit 1
     fi
     LIBS_MO="$LIBS_MO $LIB_MO"
 done
