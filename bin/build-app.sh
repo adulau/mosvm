a37d56a68343c0b1099e1f886ebe24d417724b8b
swdunlop <swdunlop@ephemeralsecurity.com>
2006-06-08 01:15:50 +0200

    Patched bin/build-app.sh to accept using mosc.
    darcs-hash:20060607231550-68f70-f0aebe4af38c84ae042c35a832a9fc1017c2fb85

--- bin/build-app.sh
+++ bin/build-app.sh
@@ -7,13 +7,16 @@ STUB=$1
 PROG=$2
 OUTP=$3
 
-if [ x$USE_GUILE != x ]; then
+if [ x$USE_MOSVM != x ]; then
+    DO_SCHEME="bin/mosvm "
+elif [ x$USE_GUILE != x ]; then
     DO_SCHEME="guile --debug -l $LIB/run-guile.scm -e guile-main -s"
 elif [ x$USE_MZSCHEME != x ]; then
     DO_SCHEME="mzscheme -M errortrace -f $LIB/run-mzscheme.scm -C"
 else
-    echo "You must define USE_GUILE, USE_MZSCHEME or MOSC prior to invoking"
-    echo "build-app.sh"
+    echo "You must define USE_GUILE, USE_MZSCHEME or USE_MOSVM prior to "
+    echo "invoking build-app.sh -- a scheme or mosquito evaluator is "
+    echo "required for analyzing dependencies of the application."
     exit 1
 fi
 
@@ -30,10 +33,15 @@ for DEP in $DEPS; do
     DEP_MO=$DEP.mo
     DEP_MA=$DEP.ma
     if [ ! -s $DEP_MO ]||[ $DEP_MS -nt $DEP_MO ]; then
-        echo "Compiling $DEP_MS to $DEP_MA.."
-        cd $BASE && $DO_SCHEME $BIN/mosc.ms $DEP || exit 1
-        echo "Assembling $DEP_MA to $DEP_MO.."
-        cd $BASE && $BIN/mosasm $DEP_MA $DEP_MO || exit 1
+        if [ x$USE_MOSC != x ]; then
+            echo "Compiling $DEP_MS to $DEP_MO.."
+            cd $BASE && bin/mosc $DEP_MS
+        else
+            echo "Compiling $DEP_MS to $DEP_MA.."
+            cd $BASE && $DO_SCHEME bin/mosc.ms $DEP_MS || exit 1
+            echo "Assembling $DEP_MA to $DEP_MO.."
+            cd $BASE && $BIN/mosasm $DEP_MA $DEP_MO || exit 1
+        fi
         RE_GLUE="yes"
     elif [ x$RE_GLUE = x ]&&[ $DEP_MO -nt $OUTP ]; then
         RE_GLUE="yes"
